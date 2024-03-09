b59f44120aff3ce4761246dcf36be35f2e8f1bc8
neil51 <neilreon@gmail.com>
2024-03-09 20:06:03 +0100

    Fixed site in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "htps://auxilio.vercel.app",
+  site: "https://auxilio.vercel.app",
   integrations: [tailwind(), mdx(), sitemap()],
   base: '/'
 });
